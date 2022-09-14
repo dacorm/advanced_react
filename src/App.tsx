ec8e2379b32f962409ae6fd85c86a0f86ffa5ea2
Utkin Denis <stolen.youth@yandex.ru>
2022-09-15 00:41:47 +0700

    feat: webpack complete router setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, {Suspense} from 'react';
+import './index.scss'
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Link to={"/about"}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    )
+        ;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import { render } from 'react-dom';
-import {Counter} from "./components/Counter";
+import {render} from 'react-dom';
+import App from "./App";
+import {BrowserRouter} from "react-router-dom";
 
 render(
-    <div>
-        <Counter />
-    </div>,
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
     document.getElementById('root')
 )
